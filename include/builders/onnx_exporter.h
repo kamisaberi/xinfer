6928ff1b11b34f01f5e18fed3b89c94eb6276aef
kamran saberifard <kamisaberi@gmail.com>
2025-08-22 03:31:57 +0330

    onnx_exporter.h files created

--- include/builders/onnx_exporter.h
+++ include/builders/onnx_exporter.h
@@ -1,8 +1,22 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef ONNX_EXPORTER_H
-#define ONNX_EXPORTER_H
 
-#endif //ONNX_EXPORTER_H
+#include <string>
+#include <vector>
+#include <xtorch/nn/module.h> // Dependency on xTorch's base module
+#include "engine_builder.h" // For InputSpec
+
+namespace xinfer::builders {
+
+    /**
+     * @brief Exports a trained xTorch model to the ONNX format.
+     * @param model A reference to the trained xTorch model.
+     * @param input_specs A vector describing the shapes and names of the input tensors.
+     * @param output_path The path to save the resulting .onnx file.
+     * @return True if export was successful, false otherwise.
+     */
+    bool export_to_onnx(xt::Module& model,
+                        const std::vector<InputSpec>& input_specs,
+                        const std::string& output_path);
+
+} // namespace xinfer::builders

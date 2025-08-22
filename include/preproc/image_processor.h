9add2d9d5ffdd937599bf817050d0f03e61c3b27
kamran saberifard <kamisaberi@gmail.com>
2025-08-22 18:37:09 +0330

    image_procossor.h files created

--- include/preproc/image_processor.h
+++ include/preproc/image_processor.h
@@ -1,8 +1,32 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef IMAGE_PROCESSOR_H
-#define IMAGE_PROCESSOR_H
+#include <opencv2/opencv.hpp>
+#include "../core/tensor.h"
 
-#endif //IMAGE_PROCESSOR_H
+namespace xinfer::preproc {
+
+    /**
+     * @class ImageProcessor
+     * @brief Performs an entire image pre-processing pipeline in a single, fused CUDA kernel.
+     */
+    class ImageProcessor {
+    public:
+        // Configures the processing pipeline (target size, normalization values, etc.)
+        ImageProcessor(int width, int height,
+                       const std::vector<float>& mean,
+                       const std::vector<float>& std);
+        ~ImageProcessor();
+
+        /**
+         * @brief Processes a CPU image and returns a GPU tensor.
+         * @param cpu_image An OpenCV BGR image.
+         * @param output_tensor A pre-allocated GPU tensor to write the result into.
+         */
+        void process(const cv::Mat& cpu_image, core::Tensor& output_tensor);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::preproc

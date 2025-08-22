f4b26acc28f453956a1c4fb13f800e1679941e14
kamran saberifard <kamisaberi@gmail.com>
2025-08-22 18:37:59 +0330

    image_procossor.h files created

--- include/postproc/detection.h
+++ include/postproc/detection.h
@@ -1,8 +1,23 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef DETECTION_H
-#define DETECTION_H
+#include <vector>
+#include "../core/tensor.h"
 
-#endif //DETECTION_H
+namespace xinfer::postproc {
+
+    struct BoundingBox { /* x1, y1, x2, y2, class_id, confidence, ... */ };
+
+    /**
+     * @brief Performs Non-Maximum Suppression (NMS) on raw detection outputs on the GPU.
+     * @param raw_boxes A GPU tensor of shape [N, 4] with box coordinates.
+     * @param raw_scores A GPU tensor of shape [N, num_classes] with scores.
+     * @param score_threshold The confidence score threshold.
+     * @param iou_threshold The IoU threshold for suppression.
+     * @return A vector of BoundingBox structs, copied back to the CPU.
+     */
+    std::vector<BoundingBox> nms(const core::Tensor& raw_boxes,
+                                 const core::Tensor& raw_scores,
+                                 float score_threshold,
+                                 float iou_threshold);
+
+} // namespace xinfer::postproc

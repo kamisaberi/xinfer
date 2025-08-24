421b23ee5fae1be80e6f715668a5296d6e526a45
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 17:58:08 +0330

    voice_converter.h created

--- include/zoo/generative/voice_converter.h
+++ include/zoo/generative/voice_converter.h
@@ -1,8 +1,36 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef VOICE_CONVERTER_H
-#define VOICE_CONVERTER_H
+#include <string>
+#include <vector>
+#include <memory>
 
-#endif //VOICE_CONVERTER_H
+namespace xinfer::core { class Tensor; }
+
+namespace xinfer::zoo::generative {
+
+    using AudioWaveform = std::vector<float>;
+
+    struct VoiceConverterConfig {
+        std::string engine_path;
+        // Add paths to speaker embedding files, vocabs, etc.
+    };
+
+    class VoiceConverter {
+    public:
+        explicit VoiceConverter(const VoiceConverterConfig& config);
+        ~VoiceConverter();
+
+        VoiceConverter(const VoiceConverter&) = delete;
+        VoiceConverter& operator=(const VoiceConverter&) = delete;
+        VoiceConverter(VoiceConverter&&) noexcept;
+        VoiceConverter& operator=(VoiceConverter&&) noexcept;
+
+        AudioWaveform predict(const AudioWaveform& source_audio, const AudioWaveform& target_voice_sample);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

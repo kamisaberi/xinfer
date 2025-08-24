e871feacbb8670e5a0efc5ba6b10c3a4da74e740
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 15:28:43 +0330

    anomaly_detector.h created

--- include/zoo/vision/anomaly_detector.h
+++ include/zoo/vision/anomaly_detector.h
@@ -1,1 +1,46 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::vision {
+
+    struct AnomalyResult {
+        bool is_anomaly;
+        float anomaly_score;
+        cv::Mat anomaly_map;
+    };
+
+    struct AnomalyDetectorConfig {
+        std::string engine_path;
+        float anomaly_threshold = 0.1f;
+        int input_width = 256;
+        int input_height = 256;
+        std::vector<float> mean = {0.5f, 0.5f, 0.5f};
+        std::vector<float> std = {0.5f, 0.5f, 0.5f};
+    };
+
+    class AnomalyDetector {
+    public:
+        explicit AnomalyDetector(const AnomalyDetectorConfig& config);
+        ~AnomalyDetector();
+
+        AnomalyDetector(const AnomalyDetector&) = delete;
+        AnomalyDetector& operator=(const AnomalyDetector&) = delete;
+        AnomalyDetector(AnomalyDetector&&) noexcept;
+        AnomalyDetector& operator=(AnomalyDetector&&) noexcept;
+
+        AnomalyResult predict(const cv::Mat& image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::vision
+

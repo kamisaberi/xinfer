2703670854a8567162fbe9350292741a5a884d68
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 18:01:11 +0330

    audio_processor.h created

--- include/postproc/audio_processor.h
+++ include/postproc/audio_processor.h
@@ -1,8 +1,38 @@
-//
-// Created by kami on 8/24/25.
-//
+#pragma once
 
-#ifndef AUDIO_PROCESSOR_H
-#define AUDIO_PROCESSOR_H
 
-#endif //AUDIO_PROCESSOR_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <include/core/tensor.h>
+
+namespace xinfer::preproc {
+
+    struct AudioProcessorConfig {
+        int sample_rate = 22050;
+        int n_fft = 1024;
+        int hop_length = 256;
+        int n_mels = 80;
+        float f_min = 0.0f;
+        float f_max = 8000.0f;
+    };
+
+    class AudioProcessor {
+    public:
+        explicit AudioProcessor(const AudioProcessorConfig& config);
+        ~AudioProcessor();
+
+        AudioProcessor(const AudioProcessor&) = delete;
+        AudioProcessor& operator=(const AudioProcessor&) = delete;
+        AudioProcessor(AudioProcessor&&) noexcept;
+        AudioProcessor& operator=(AudioProcessor&&) noexcept;
+
+        void process(const std::vector<float>& waveform, core::Tensor& out_spectrogram);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::preproc
+

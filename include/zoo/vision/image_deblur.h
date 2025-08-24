a267bf22860776246114c67919c9cd13f0cfcc50
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 16:07:05 +0330

    image_deblur.h created

--- include/zoo/vision/image_deblur.h
+++ include/zoo/vision/image_deblur.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::vision {
+
+    struct ImageDeblurConfig {
+        std::string engine_path;
+        int input_width = 256;
+        int input_height = 256;
+    };
+
+    class ImageDeblur {
+    public:
+        explicit ImageDeblur(const ImageDeblurConfig& config);
+        ~ImageDeblur();
+
+        ImageDeblur(const ImageDeblur&) = delete;
+        ImageDeblur& operator=(const ImageDeblur&) = delete;
+        ImageDeblur(ImageDeblur&&) noexcept;
+        ImageDeblur& operator=(ImageDeblur&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& blurry_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::vision
+

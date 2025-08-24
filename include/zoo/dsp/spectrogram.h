cf42b81b33919dd7a88c2023eba48aa8e18eedad
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 18:45:16 +0330

    spectrogram.h created

--- include/zoo/dsp/spectrogram.h
+++ include/zoo/dsp/spectrogram.h
@@ -1,8 +1,34 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef SPECTROGRAM_H
-#define SPECTROGRAM_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
 
-#endif //SPECTROGRAM_H
+namespace xinfer::zoo::dsp {
+
+    struct SpectrogramConfig {
+        int sample_rate = 22050;
+        int n_fft = 1024;
+        int hop_length = 256;
+    };
+
+    class Spectrogram {
+    public:
+        explicit Spectrogram(const SpectrogramConfig& config);
+        ~Spectrogram();
+
+        Spectrogram(const Spectrogram&) = delete;
+        Spectrogram& operator=(const Spectrogram&) = delete;
+        Spectrogram(Spectrogram&&) noexcept;
+        Spectrogram& operator=(Spectrogram&&) noexcept;
+
+        cv::Mat process(const std::vector<float>& waveform);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::dsp
+

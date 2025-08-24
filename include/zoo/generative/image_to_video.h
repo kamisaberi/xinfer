59477485b3886c1cd88ad6f9207813ee06ccc05b
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 17:26:54 +0330

    image_to_video.h created

--- include/zoo/generative/image_to_video.h
+++ include/zoo/generative/image_to_video.h
@@ -1,8 +1,36 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef IMAGE_TO_VIDEO_H
-#define IMAGE_TO_VIDEO_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+#include <include/core/tensor.h>
 
-#endif //IMAGE_TO_VIDEO_H
+namespace xinfer::zoo::generative {
+
+    struct ImageToVideoConfig {
+        std::string engine_path;
+        int num_frames = 16;
+        int input_width = 256;
+        int input_height = 256;
+    };
+
+    class ImageToVideo {
+    public:
+        explicit ImageToVideo(const ImageToVideoConfig& config);
+        ~ImageToVideo();
+
+        ImageToVideo(const ImageToVideo&) = delete;
+        ImageToVideo& operator=(const ImageToVideo&) = delete;
+        ImageToVideo(ImageToVideo&&) noexcept;
+        ImageToVideo& operator=(ImageToVideo&&) noexcept;
+
+        std::vector<cv::Mat> predict(const cv::Mat& start_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

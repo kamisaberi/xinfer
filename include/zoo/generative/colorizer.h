2bf35d5e5067157a0f67284515d175fd7c9bb239
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 16:53:03 +0330

    colorizer.h created

--- include/zoo/generative/colorizer.h
+++ include/zoo/generative/colorizer.h
@@ -1,8 +1,37 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef COLORIZER_H
-#define COLORIZER_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
 
-#endif //COLORIZER_H
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::generative {
+
+    struct ColorizerConfig {
+        std::string engine_path;
+        int input_width = 256;
+        int input_height = 256;
+    };
+
+    class Colorizer {
+    public:
+        explicit Colorizer(const ColorizerConfig& config);
+        ~Colorizer();
+
+        Colorizer(const Colorizer&) = delete;
+        Colorizer& operator=(const Colorizer&) = delete;
+        Colorizer(Colorizer&&) noexcept;
+        Colorizer& operator=(Colorizer&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& grayscale_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

03d6920b0f337939bf3b6cf793424f2ae53950e7
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 14:52:12 +0330

    ocr_decoder.h created

--- include/postproc/ocr_decoder.h
+++ include/postproc/ocr_decoder.h
@@ -1,8 +1,25 @@
-//
-// Created by kami on 8/24/25.
-//
+#pragma once
 
-#ifndef OCR_DECODER_H
-#define OCR_DECODER_H
+#include <include/core/tensor.h>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <tuple>
 
-#endif //OCR_DECODER_H
+namespace xinfer::postproc::ocr {
+
+    // Decodes the two-part output of a CRAFT text detection model
+    std::vector<std::vector<cv::Point2f>> decode_detection_output_craft(
+        const core::Tensor& region_scores, const core::Tensor& affinity_scores,
+        float text_threshold, float box_threshold,
+        float scale_w, float scale_h);
+
+    // Warps a detected polygonal text region into a straight rectangular patch
+    cv::Mat get_warped_text_patch(const cv::Mat& image, const std::vector<cv::Point2f>& box, int target_height);
+
+    // Decodes the output of a CRNN-style recognition model using CTC greedy decoding on the GPU
+    std::tuple<std::string, float> decode_recognition_output_ctc(
+        const core::Tensor& logits, const std::vector<char>& character_map);
+
+} // namespace xinfer::postproc::ocr
+

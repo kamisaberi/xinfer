e8609ed6abd0b327ee6f29fd9f8c57b9298a3000
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 16:41:03 +0330

    vehicle_identifier.h created

--- include/zoo/vision/vehicle_identifier.h
+++ include/zoo/vision/vehicle_identifier.h
@@ -1,1 +1,52 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::vision {
+
+    struct VehicleAttributes {
+        std::string type;
+        float type_confidence;
+        std::string color;
+        float color_confidence;
+        std::string make;
+        float make_confidence;
+        std::string model;
+        float model_confidence;
+    };
+
+    struct VehicleIdentifierConfig {
+        std::string engine_path;
+        std::string type_labels_path = "";
+        std::string color_labels_path = "";
+        std::string make_labels_path = "";
+        std::string model_labels_path = "";
+        int input_width = 224;
+        int input_height = 224;
+    };
+
+    class VehicleIdentifier {
+    public:
+        explicit VehicleIdentifier(const VehicleIdentifierConfig& config);
+        ~VehicleIdentifier();
+
+        VehicleIdentifier(const VehicleIdentifier&) = delete;
+        VehicleIdentifier& operator=(const VehicleIdentifier&) = delete;
+        VehicleIdentifier(VehicleIdentifier&&) noexcept;
+        VehicleIdentifier& operator=(VehicleIdentifier&&) noexcept;
+
+        VehicleAttributes predict(const cv::Mat& vehicle_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::vision
+

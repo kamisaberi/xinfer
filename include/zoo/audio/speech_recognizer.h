767bb45080d4f7246f422f7cf29d9cf5ae4af72b
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 18:30:25 +0330

    speech_recognizer.cpp created

--- include/zoo/audio/speech_recognizer.h
+++ include/zoo/audio/speech_recognizer.h
@@ -1,8 +1,39 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef SPEECH_RECOGNIZER_H
-#define SPEECH_RECOGNIZER_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <include/preproc/audio_processor.h>
 
-#endif //SPEECH_RECOGNIZER_H
+namespace xinfer::zoo::audio {
+
+    struct TranscriptionResult {
+        std::string text;
+        float confidence;
+    };
+
+    struct SpeechRecognizerConfig {
+        std::string engine_path;
+        std::string character_map_path;
+        preproc::AudioProcessorConfig audio_config;
+    };
+
+    class SpeechRecognizer {
+    public:
+        explicit SpeechRecognizer(const SpeechRecognizerConfig& config);
+        ~SpeechRecognizer();
+
+        SpeechRecognizer(const SpeechRecognizer&) = delete;
+        SpeechRecognizer& operator=(const SpeechRecognizer&) = delete;
+        SpeechRecognizer(SpeechRecognizer&&) noexcept;
+        SpeechRecognizer& operator=(SpeechRecognizer&&) noexcept;
+
+        TranscriptionResult predict(const std::vector<float>& waveform);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::audio
+

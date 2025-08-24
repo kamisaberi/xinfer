18bbc749a68491493f28454dd6f98cb8b309d2f3
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 18:42:56 +0330

    signal_filter.h created

--- include/zoo/dsp/signal_filter.h
+++ include/zoo/dsp/signal_filter.h
@@ -1,8 +1,41 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef SIGNAL_FILTER_H
-#define SIGNAL_FILTER_H
+#include <string>
+#include <vector>
+#include <memory>
 
-#endif //SIGNAL_FILTER_H
+namespace xinfer::zoo::dsp {
+
+    enum class FilterType {
+        LOW_PASS,
+        HIGH_PASS,
+        BAND_PASS
+    };
+
+    struct SignalFilterConfig {
+        FilterType type;
+        int sample_rate = 44100;
+        float cutoff_freq1 = 1000.0f;
+        float cutoff_freq2 = 0.0f; // Used for band-pass
+        int filter_length = 129; // Should be odd
+    };
+
+    class SignalFilter {
+    public:
+        explicit SignalFilter(const SignalFilterConfig& config);
+        ~SignalFilter();
+
+        SignalFilter(const SignalFilter&) = delete;
+        SignalFilter& operator=(const SignalFilter&) = delete;
+        SignalFilter(SignalFilter&&) noexcept;
+        SignalFilter& operator=(SignalFilter&&) noexcept;
+
+        std::vector<float> process(const std::vector<float>& input_signal);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::dsp
+

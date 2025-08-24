cd6d03fe23da1ce9ab05bfb619143842041c7b83
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 17:29:18 +0330

    inpainter.h created

--- include/zoo/generative/inpainter.h
+++ include/zoo/generative/inpainter.h
@@ -1,8 +1,38 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef INPAINTER_H
-#define INPAINTER_H
 
-#endif //INPAINTER_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::generative {
+
+    struct InpainterConfig {
+        std::string engine_path;
+        int input_width = 512;
+        int input_height = 512;
+    };
+
+    class Inpainter {
+    public:
+        explicit Inpainter(const InpainterConfig& config);
+        ~Inpainter();
+
+        Inpainter(const Inpainter&) = delete;
+        Inpainter& operator=(const Inpainter&) = delete;
+        Inpainter(Inpainter&&) noexcept;
+        Inpainter& operator=(Inpainter&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& image, const cv::Mat& mask);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

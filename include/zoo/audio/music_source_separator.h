64c192f3de50c3e7c707c77e81327e820fecfa6f
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 18:25:01 +0330

    music_source_separator.h created

--- include/zoo/audio/music_source_separator.h
+++ include/zoo/audio/music_source_separator.h
@@ -1,8 +1,37 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef MUSIC_SOURCE_SEPARATOR_H
-#define MUSIC_SOURCE_SEPARATOR_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <map>
+#include <include/preproc/audio_processor.h>
 
-#endif //MUSIC_SOURCE_SEPARATOR_H
+namespace xinfer::zoo::audio {
+
+    using AudioWaveform = std::vector<float>;
+
+    struct MusicSourceSeparatorConfig {
+        std::string engine_path;
+        preproc::AudioProcessorConfig audio_config;
+        std::vector<std::string> source_names = {"drums", "bass", "other", "vocals"};
+    };
+
+    class MusicSourceSeparator {
+    public:
+        explicit MusicSourceSeparator(const MusicSourceSeparatorConfig& config);
+        ~MusicSourceSeparator();
+
+        MusicSourceSeparator(const MusicSourceSeparator&) = delete;
+        MusicSourceSeparator& operator=(const MusicSourceSeparator&) = delete;
+        MusicSourceSeparator(MusicSourceSeparator&&) noexcept;
+        MusicSourceSeparator& operator=(MusicSourceSeparator&&) noexcept;
+
+        std::map<std::string, AudioWaveform> predict(const std::vector<float>& mixed_waveform);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::audio
+

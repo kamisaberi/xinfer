b0a3a60146afac358a38488cd8faf385e0f60d67
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 16:18:59 +0330

    low_light_enhancer.h created

--- include/zoo/vision/low_light_enhancer.h
+++ include/zoo/vision/low_light_enhancer.h
@@ -1,1 +1,38 @@
 #pragma once
+
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::vision {
+
+    struct LowLightEnhancerConfig {
+        std::string engine_path;
+        int input_width = 512;
+        int input_height = 512;
+    };
+
+    class LowLightEnhancer {
+    public:
+        explicit LowLightEnhancer(const LowLightEnhancerConfig& config);
+        ~LowLightEnhancer();
+
+        LowLightEnhancer(const LowLightEnhancer&) = delete;
+        LowLightEnhancer& operator=(const LowLightEnhancer&) = delete;
+        LowLightEnhancer(LowLightEnhancer&&) noexcept;
+        LowLightEnhancer& operator=(LowLightEnhancer&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& low_light_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::vision
+

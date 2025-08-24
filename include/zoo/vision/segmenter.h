fe0adbe8093af03a0180e02817746a9cd78084c4
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 16:33:56 +0330

    segmenter.h created

--- include/zoo/vision/segmenter.h
+++ include/zoo/vision/segmenter.h
@@ -1,1 +1,38 @@
 #pragma once
+
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::vision {
+
+    struct SegmenterConfig {
+        std::string engine_path;
+        int input_width = 512;
+        int input_height = 512;
+    };
+
+    class Segmenter {
+    public:
+        explicit Segmenter(const SegmenterConfig& config);
+        ~Segmenter();
+
+        Segmenter(const Segmenter&) = delete;
+        Segmenter& operator=(const Segmenter&) = delete;
+        Segmenter(Segmenter&&) noexcept;
+        Segmenter& operator=(Segmenter&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::vision
+

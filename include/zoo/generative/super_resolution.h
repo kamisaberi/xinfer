f817ab09f49576dfb20000f0311b1356c6704383
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 17:48:35 +0330

    super_resolution.h created

--- include/zoo/generative/super_resolution.h
+++ include/zoo/generative/super_resolution.h
@@ -1,8 +1,38 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef SUPER_RESOLUTION_H
-#define SUPER_RESOLUTION_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
 
-#endif //SUPER_RESOLUTION_H
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::generative {
+
+    struct SuperResolutionConfig {
+        std::string engine_path;
+        int upscale_factor = 4;
+        int input_width = 128;
+        int input_height = 128;
+    };
+
+    class SuperResolution {
+    public:
+        explicit SuperResolution(const SuperResolutionConfig& config);
+        ~SuperResolution();
+
+        SuperResolution(const SuperResolution&) = delete;
+        SuperResolution& operator=(const SuperResolution&) = delete;
+        SuperResolution(SuperResolution&&) noexcept;
+        SuperResolution& operator=(SuperResolution&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& low_res_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

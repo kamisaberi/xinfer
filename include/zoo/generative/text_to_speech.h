509af0d6805b0968288509ccd89baf133a8f8a1d
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 17:53:18 +0330

    text_to_speech.h created

--- include/zoo/generative/text_to_speech.h
+++ include/zoo/generative/text_to_speech.h
@@ -1,8 +1,38 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef TEXT_TO_SPEECH_H
-#define TEXT_TO_SPEECH_H
 
-#endif //TEXT_TO_SPEECH_H
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace xinfer::core { class Tensor; }
+
+namespace xinfer::zoo::generative {
+
+    using AudioWaveform = std::vector<float>;
+
+    struct TextToSpeechConfig {
+        std::string spectrogram_engine_path;
+        std::string vocoder_engine_path;
+        // Add paths to tokenizer vocabs, etc.
+    };
+
+    class TextToSpeech {
+    public:
+        explicit TextToSpeech(const TextToSpeechConfig& config);
+        ~TextToSpeech();
+
+        TextToSpeech(const TextToSpeech&) = delete;
+        TextToSpeech& operator=(const TextToSpeech&) = delete;
+        TextToSpeech(TextToSpeech&&) noexcept;
+        TextToSpeech& operator=(TextToSpeech&&) noexcept;
+
+        AudioWaveform predict(const std::string& text);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

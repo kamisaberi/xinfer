091ea4e3b2b1ae73a73a747d4356ce6c792e73ff
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 18:22:28 +0330

    language_identifier.h created

--- include/zoo/audio/language_identifier.h
+++ include/zoo/audio/language_identifier.h
@@ -1,8 +1,40 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef LANGUAGE_IDENTIFIER_H
-#define LANGUAGE_IDENTIFIER_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <include/preproc/audio_processor.h>
 
-#endif //LANGUAGE_IDENTIFIER_H
+namespace xinfer::zoo::audio {
+
+    struct LanguageIdentificationResult {
+        int lang_id;
+        float confidence;
+        std::string language_code; // e.g., "en", "es", "fr"
+    };
+
+    struct LanguageIdentifierConfig {
+        std::string engine_path;
+        std::string labels_path = "";
+        preproc::AudioProcessorConfig audio_config;
+    };
+
+    class LanguageIdentifier {
+    public:
+        explicit LanguageIdentifier(const LanguageIdentifierConfig& config);
+        ~LanguageIdentifier();
+
+        LanguageIdentifier(const LanguageIdentifier&) = delete;
+        LanguageIdentifier& operator=(const LanguageIdentifier&) = delete;
+        LanguageIdentifier(LanguageIdentifier&&) noexcept;
+        LanguageIdentifier& operator=(LanguageIdentifier&&) noexcept;
+
+        LanguageIdentificationResult predict(const std::vector<float>& waveform);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::audio
+

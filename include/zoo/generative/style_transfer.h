032a594021566694699446a5c3b444ac7166f4a7
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 17:35:06 +0330

    style_transfer.h created

--- include/zoo/generative/style_transfer.h
+++ include/zoo/generative/style_transfer.h
@@ -1,8 +1,37 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef STYLE_TRANSFER_H
-#define STYLE_TRANSFER_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
 
-#endif //STYLE_TRANSFER_H
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::generative {
+
+    struct StyleTransferConfig {
+        std::string engine_path;
+        int input_width = 512;
+        int input_height = 512;
+    };
+
+    class StyleTransfer {
+    public:
+        explicit StyleTransfer(const StyleTransferConfig& config);
+        ~StyleTransfer();
+
+        StyleTransfer(const StyleTransfer&) = delete;
+        StyleTransfer& operator=(const StyleTransfer&) = delete;
+        StyleTransfer(StyleTransfer&&) noexcept;
+        StyleTransfer& operator=(StyleTransfer&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& content_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

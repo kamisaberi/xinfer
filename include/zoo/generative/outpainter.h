7d9adbf748edbdcc7cef4a1847662b67acc32884
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 17:32:44 +0330

    outpainter.h created

--- include/zoo/generative/outpainter.h
+++ include/zoo/generative/outpainter.h
@@ -1,8 +1,37 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef OUTPAINTER_H
-#define OUTPAINTER_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
 
-#endif //OUTPAINTER_H
+namespace xinfer::core { class Tensor; }
+namespace xinfer::preproc { class ImageProcessor; }
+
+namespace xinfer::zoo::generative {
+
+    struct OutpainterConfig {
+        std::string engine_path;
+        int input_width = 512;
+        int input_height = 512;
+    };
+
+    class Outpainter {
+    public:
+        explicit Outpainter(const OutpainterConfig& config);
+        ~Outpainter();
+
+        Outpainter(const Outpainter&) = delete;
+        Outpainter& operator=(const Outpainter&) = delete;
+        Outpainter(Outpainter&&) noexcept;
+        Outpainter& operator=(Outpainter&&) noexcept;
+
+        cv::Mat predict(const cv::Mat& image, int top, int right, int bottom, int left);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::generative
+

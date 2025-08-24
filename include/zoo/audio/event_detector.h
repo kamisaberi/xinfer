1fd76444582010a89269d237cf5332f1fa162b14
kamran saberifard <kamisaberi@gmail.com>
2025-08-24 18:20:06 +0330

    event_detector.h created

--- include/zoo/audio/event_detector.h
+++ include/zoo/audio/event_detector.h
@@ -1,8 +1,43 @@
-//
-// Created by kami on 8/16/25.
-//
+#pragma once
 
-#ifndef EVENT_DETECTOR_H
-#define EVENT_DETECTOR_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <include/preproc/audio_processor.h>
 
-#endif //EVENT_DETECTOR_H
+namespace xinfer::zoo::audio {
+
+    struct AudioEvent {
+        int class_id;
+        float confidence;
+        std::string label;
+        float start_time_seconds;
+        float end_time_seconds;
+    };
+
+    struct EventDetectorConfig {
+        std::string engine_path;
+        std::string labels_path = "";
+        float event_threshold = 0.5f;
+        preproc::AudioProcessorConfig audio_config;
+    };
+
+    class EventDetector {
+    public:
+        explicit EventDetector(const EventDetectorConfig& config);
+        ~EventDetector();
+
+        EventDetector(const EventDetector&) = delete;
+        EventDetector& operator=(const EventDetector&) = delete;
+        EventDetector(EventDetector&&) noexcept;
+        EventDetector& operator=(EventDetector&&) noexcept;
+
+        std::vector<AudioEvent> predict(const std::vector<float>& waveform);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::audio
+

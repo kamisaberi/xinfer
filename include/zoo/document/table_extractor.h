8c02509e090c16caf0cd7242a8745cd2545f37ec
kamran saberifard <kamisaberi@gmail.com>
2025-08-27 02:26:32 +0330

    new zoo models created

--- include/zoo/document/table_extractor.h
+++ include/zoo/document/table_extractor.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+#include <include/zoo/vision/ocr.h>
+
+namespace xinfer::zoo::document {
+
+    using Table = std::vector<std::vector<std::string>>;
+
+    struct TableExtractorConfig {
+        std::string structure_engine_path;
+        std::string ocr_config_path; // Path to a config file for the OCR model
+    };
+
+    class TableExtractor {
+    public:
+        explicit TableExtractor(const TableExtractorConfig& config);
+        ~TableExtractor();
+
+        TableExtractor(const TableExtractor&) = delete;
+        TableExtractor& operator=(const TableExtractor&) = delete;
+        TableExtractor(TableExtractor&&) noexcept;
+        TableExtractor& operator=(TableExtractor&&) noexcept;
+
+        Table predict(const cv::Mat& table_image);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::document
+

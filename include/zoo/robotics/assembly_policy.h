de7e8f6389a5be2777b0b4427ec239289614b2ce
kamran saberifard <kamisaberi@gmail.com>
2025-08-27 22:56:02 +0330

    new zoo models created

--- include/zoo/robotics/assembly_policy.h
+++ include/zoo/robotics/assembly_policy.h
@@ -1,8 +1,36 @@
-//
-// Created by kami on 8/27/25.
-//
+#pragma once
 
-#ifndef ASSEMBLY_POLICY_H
-#define ASSEMBLY_POLICY_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+#include <include/core/tensor.h>
+#include <include/zoo/rl/policy.h>
+#include <include/zoo/vision/classifier.h> // Example for a vision encoder
 
-#endif //ASSEMBLY_POLICY_H
+namespace xinfer::zoo::robotics {
+
+    struct AssemblyPolicyConfig {
+        std::string vision_encoder_engine_path;
+        std::string policy_engine_path;
+    };
+
+    class AssemblyPolicy {
+    public:
+        explicit AssemblyPolicy(const AssemblyPolicyConfig& config);
+        ~AssemblyPolicy();
+
+        AssemblyPolicy(const AssemblyPolicy&) = delete;
+        AssemblyPolicy& operator=(const AssemblyPolicy&) = delete;
+        AssemblyPolicy(AssemblyPolicy&&) noexcept;
+        AssemblyPolicy& operator=(AssemblyPolicy&&) noexcept;
+
+        std::vector<float> predict(const cv::Mat& robot_camera_view, const std::vector<float>& robot_joint_states);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::robotics
+

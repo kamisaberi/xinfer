916c5661ebcfdd53f6ce17c9814c0486c7324bae
kamran saberifard <kamisaberi@gmail.com>
2025-08-27 23:18:46 +0330

    new zoo models created

--- include/zoo/gaming/npc_behavior_policy.h
+++ include/zoo/gaming/npc_behavior_policy.h
@@ -1,8 +1,33 @@
-//
-// Created by kami on 8/27/25.
-//
+#pragma once
 
-#ifndef NPC_BEHAVIOR_POLICY_H
-#define NPC_BEHAVIOR_POLICY_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <include/core/tensor.h>
+#include <include/zoo/rl/policy.h>
 
-#endif //NPC_BEHAVIOR_POLICY_H
+namespace xinfer::zoo::gaming {
+
+    struct NPCBehaviorPolicyConfig {
+        std::string engine_path;
+    };
+
+    class NPCBehaviorPolicy {
+    public:
+        explicit NPCBehaviorPolicy(const NPCBehaviorPolicyConfig& config);
+        ~NPCBehaviorPolicy();
+
+        NPCBehaviorPolicy(const NPCBehaviorPolicy&) = delete;
+        NPCBehaviorPolicy& operator=(const NPCBehaviorPolicy&) = delete;
+        NPCBehaviorPolicy(NPCBehaviorPolicy&&) noexcept;
+        NPCBehaviorPolicy& operator=(NPCBehaviorPolicy&&) noexcept;
+
+        core::Tensor predict_batch(const core::Tensor& npc_state_batch);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::gaming
+

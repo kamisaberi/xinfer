76f35ac9cba64582eb9829ab87fabec2e7ecfb8c
kamran saberifard <kamisaberi@gmail.com>
2025-08-27 22:59:41 +0330

    new zoo models created

--- include/zoo/drones/navigation_policy.h
+++ include/zoo/drones/navigation_policy.h
@@ -1,8 +1,38 @@
-//
-// Created by kami on 8/27/25.
-//
+#pragma once
 
-#ifndef NAVIGATION_POLICY_H
-#define NAVIGATION_POLICY_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <opencv2/opencv.hpp>
+#include <include/core/tensor.h>
+#include <include/zoo/rl/policy.h>
 
-#endif //NAVIGATION_POLICY_H
+namespace xinfer::zoo::drones {
+
+    struct NavigationAction {
+        float roll, pitch, yaw, thrust;
+    };
+
+    struct NavigationPolicyConfig {
+        std::string engine_path;
+    };
+
+    class NavigationPolicy {
+    public:
+        explicit NavigationPolicy(const NavigationPolicyConfig& config);
+        ~NavigationPolicy();
+
+        NavigationPolicy(const NavigationPolicy&) = delete;
+        NavigationPolicy& operator=(const NavigationPolicy&) = delete;
+        NavigationPolicy(NavigationPolicy&&) noexcept;
+        NavigationPolicy& operator=(NavigationPolicy&&) noexcept;
+
+        NavigationAction predict(const cv::Mat& depth_image, const std::vector<float>& drone_state);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::drones
+

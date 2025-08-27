598249bfe9807cc90bf5ae47eee0a6516f8fe35e
kamran saberifard <kamisaberi@gmail.com>
2025-08-27 23:10:40 +0330

    new zoo models created

--- include/zoo/hft/order_execution_policy.h
+++ include/zoo/hft/order_execution_policy.h
@@ -1,8 +1,45 @@
-//
-// Created by kami on 8/27/25.
-//
+#pragma once
 
-#ifndef ORDER_EXECUTION_POLICY_H
-#define ORDER_EXECUTION_POLICY_H
+#include <string>
+#include <vector>
+#include <memory>
+#include <include/core/tensor.h>
+#include <include/zoo/rl/policy.h>
 
-#endif //ORDER_EXECUTION_POLICY_H
+namespace xinfer::zoo::hft {
+
+    enum class OrderActionType {
+        DO_NOTHING,
+        PLACE_BUY,
+        PLACE_SELL
+    };
+
+    struct OrderExecutionAction {
+        OrderActionType action;
+        float volume;
+        float price_level;
+    };
+
+    struct OrderExecutionPolicyConfig {
+        std::string engine_path;
+    };
+
+    class OrderExecutionPolicy {
+    public:
+        explicit OrderExecutionPolicy(const OrderExecutionPolicyConfig& config);
+        ~OrderExecutionPolicy();
+
+        OrderExecutionPolicy(const OrderExecutionPolicy&) = delete;
+        OrderExecutionPolicy& operator=(const OrderExecutionPolicy&) = delete;
+        OrderExecutionPolicy(OrderExecutionPolicy&&) noexcept;
+        OrderExecutionPolicy& operator=(OrderExecutionPolicy&&) noexcept;
+
+        OrderExecutionAction predict(const core::Tensor& market_state_tensor);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::zoo::hft
+

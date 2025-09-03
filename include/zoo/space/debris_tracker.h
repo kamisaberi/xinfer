270598fcf260ce08de0261262e479f62f56844b8
kamran saberifard <kamisaberi@gmail.com>
2025-09-03 22:39:34 +0330

    some models created

---
+++ include/zoo/space/debris_tracker.h
@@ -0,0 +1,8 @@
+//
+// Created by kami on 9/3/25.
+//
+
+#ifndef DEBRIS_TRACKER_H
+#define DEBRIS_TRACKER_H
+
+#endif //DEBRIS_TRACKER_H
--- include/zoo/space/deepfake_detector.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by kami on 9/3/25.
-//
-
-#ifndef DEEPFAKE_DETECTOR_H
-#define DEEPFAKE_DETECTOR_H
-
-#endif //DEEPFAKE_DETECTOR_H

2452933b8c4477dde8e80b5d4390f747e337c013
= <kamisaberi@gmail.com>
2025-12-22 01:31:10 +0330

    readme updated

--- include/backends/ambarella_cv/types.h
+++ include/backends/ambarella_cv/types.h
@@ -1,8 +1,20 @@
-//
-// Created by kami on 12/22/2025.
-//
+#pragma once
 
-#ifndef XINFER_TYPES_H
-#define XINFER_TYPES_H
+namespace xinfer::backends::ambarella {
 
-#endif //XINFER_TYPES_H
+    // Ambarella CVFlow specific error codes
+    enum class CavalryError {
+        SUCCESS = 0,
+        DAG_LOAD_FAILED = -1,
+        DMA_FAILURE = -2,
+        VP_HANG = -3
+    };
+
+    // CVFlow supports specific fixed-point formats
+    enum class cv_precision_t {
+        FIX_8 = 0,  // Standard INT8
+        FIX_16 = 1, // INT16
+        FIX_4 = 2   // 4-bit packed (Ultra fast)
+    };
+
+}

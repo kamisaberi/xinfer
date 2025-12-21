73d24eff2a5d4180cb49d2b57af49de2ca9ed442
= <kamisaberi@gmail.com>
2025-12-22 02:55:00 +0330

    readme updated

--- include/backends/google_tpu/types.h
+++ include/backends/google_tpu/types.h
@@ -1,8 +1,25 @@
-//
-// Created by kami on 12/22/2025.
-//
+#pragma once
 
-#ifndef XINFER_TYPES_H
-#define XINFER_TYPES_H
+namespace xinfer::backends::google_tpu {
 
-#endif //XINFER_TYPES_H
+    /**
+     * @brief Edge TPU Device Type
+     * Specifies which interface the TPU is connected through.
+     */
+    enum class TpuDeviceType {
+        ANY = 0, // Pick the first available TPU
+        USB = 1, // Only look for USB Accelerators
+        PCI = 2  // Only look for PCIe/M.2 cards
+    };
+
+    /**
+     * @brief TPU Power Frequency
+     * Note: High performance mode requires adequate cooling.
+     */
+    enum class TpuPerformanceMode {
+        STANDARD = 0,
+        HIGH = 1, // "Max" frequency (requires active cooling)
+        LOW = 2
+    };
+
+} // namespace xinfer::backends::google_tpu

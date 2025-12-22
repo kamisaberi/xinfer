54e0903fb1250b7d0bda95fffbd4516a0e13a266
= <kamisaberi@gmail.com>
2025-12-23 00:46:55 +0330

    readme updated

--- include/compiler/drivers/vbx_driver.h
+++ include/compiler/drivers/vbx_driver.h
@@ -1,8 +1,29 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
 
-#ifndef XINFER_VBX_DRIVER_H
-#define XINFER_VBX_DRIVER_H
+#include <xinfer/compiler/base_compiler.h>
 
-#endif //XINFER_VBX_DRIVER_H
+namespace xinfer::compiler {
+
+    /**
+     * @brief Microchip VectorBlox Compiler Driver
+     *
+     * Wraps the VectorBlox SDK to convert models into binary blobs
+     * compatible with the VectorBlox CNN IP Core on PolarFire FPGAs.
+     *
+     * Flow:
+     * 1. (Optional) Convert ONNX -> VNNX (VectorBlox Intermediate).
+     * 2. Compile VNNX -> BLOB (Hardware Instructions).
+     *
+     * Critical Param: 'CORE' (e.g., V1000) must match the FPGA bitstream.
+     */
+    class VectorBloxDriver : public ICompiler {
+    public:
+        bool compile(const CompileConfig& config) override;
+        bool validate_environment() override;
+
+        std::string get_name() const override {
+            return "Microchip VectorBlox SDK Compiler";
+        }
+    };
+
+} // namespace xinfer::compiler

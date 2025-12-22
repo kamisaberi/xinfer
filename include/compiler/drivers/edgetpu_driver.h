375f344dbc8d9879075a45c8c1caebfef835240a
= <kamisaberi@gmail.com>
2025-12-23 00:07:39 +0330

    readme updated

--- include/compiler/drivers/edgetpu_driver.h
+++ include/compiler/drivers/edgetpu_driver.h
@@ -1,8 +1,26 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
 
-#ifndef XINFER_EDGETPU_DRIVER_H
-#define XINFER_EDGETPU_DRIVER_H
+#include <xinfer/compiler/base_compiler.h>
 
-#endif //XINFER_EDGETPU_DRIVER_H
+namespace xinfer::compiler {
+
+    /**
+     * @brief Google Edge TPU Compiler Driver
+     *
+     * Wraps the 'edgetpu_compiler' CLI tool.
+     *
+     * Limitations:
+     * 1. Input MUST be a fully INT8 quantized .tflite file.
+     * 2. Does not support ONNX directly (user must convert ONNX -> TF -> TFLite first).
+     */
+    class EdgeTpuDriver : public ICompiler {
+    public:
+        bool compile(const CompileConfig& config) override;
+        bool validate_environment() override;
+
+        std::string get_name() const override {
+            return "Google Edge TPU Compiler";
+        }
+    };
+
+} // namespace xinfer::compiler

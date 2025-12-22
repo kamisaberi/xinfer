9980cdef6c2f4c6c2596d402b4284026f0f6469a
= <kamisaberi@gmail.com>
2025-12-23 00:14:58 +0330

    readme updated

--- include/compiler/drivers/exynos_driver.h
+++ include/compiler/drivers/exynos_driver.h
@@ -1,8 +1,25 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
 
-#ifndef XINFER_EXYNOS_DRIVER_H
-#define XINFER_EXYNOS_DRIVER_H
+#include <xinfer/compiler/base_compiler.h>
 
-#endif //XINFER_EXYNOS_DRIVER_H
+namespace xinfer::compiler {
+
+    /**
+     * @brief Samsung Exynos Compiler Driver
+     *
+     * Wraps the Samsung ENN SDK tools (enn_converter / SCVT) to convert
+     * ONNX models into the NNC (Neural Network Container) format.
+     *
+     * Target: Exynos 9820, 990, 2100, 2200, 2400 (NPU)
+     */
+    class ExynosDriver : public ICompiler {
+    public:
+        bool compile(const CompileConfig& config) override;
+        bool validate_environment() override;
+
+        std::string get_name() const override {
+            return "Samsung ENN Compiler";
+        }
+    };
+
+} // namespace xinfer::compiler

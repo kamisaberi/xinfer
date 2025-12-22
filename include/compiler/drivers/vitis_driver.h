1faad198825e8d41d0501f2ed4b2cea068abf14f
= <kamisaberi@gmail.com>
2025-12-23 00:48:39 +0330

    readme updated

--- include/compiler/drivers/vitis_driver.h
+++ include/compiler/drivers/vitis_driver.h
@@ -1,8 +1,35 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
 
-#ifndef XINFER_VITIS_DRIVER_H
-#define XINFER_VITIS_DRIVER_H
+#include <xinfer/compiler/base_compiler.h>
 
-#endif //XINFER_VITIS_DRIVER_H
+namespace xinfer::compiler {
+
+    /**
+     * @brief AMD/Xilinx Vitis AI Compiler Driver
+     *
+     * Wraps the 'vai_c_onnx' (Vitis AI Compiler) tool.
+     *
+     * Capability:
+     * - Can run natively (if installed) or automatically via Docker.
+     * - Converts Quantized ONNX -> .xmodel (DPU Instructions).
+     *
+     * Required Params:
+     * - DPU_ARCH: Path to the arch.json file describing the FPGA hardware.
+     */
+    class VitisDriver : public ICompiler {
+    public:
+        bool compile(const CompileConfig& config) override;
+        bool validate_environment() override;
+
+        std::string get_name() const override {
+            return "AMD Vitis AI Compiler (vai_c)";
+        }
+
+    private:
+        /**
+         * @brief Checks if we are currently running inside the Vitis Docker container.
+         */
+        bool is_native_tool_available();
+    };
+
+} // namespace xinfer::compiler

6ec4db2be29aebd597136fee20d4384f1bed0912
= <kamisaberi@gmail.com>
2025-12-23 00:18:14 +0330

    readme updated

--- include/compiler/drivers/hailo_driver.h
+++ include/compiler/drivers/hailo_driver.h
@@ -1,8 +1,31 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
 
-#ifndef XINFER_HAILO_DRIVER_H
-#define XINFER_HAILO_DRIVER_H
+#include <xinfer/compiler/base_compiler.h>
 
-#endif //XINFER_HAILO_DRIVER_H
+namespace xinfer::compiler {
+
+    /**
+     * @brief Hailo Dataflow Compiler Driver
+     *
+     * Wraps the 'hailo_sdk_client' Python API to convert ONNX models
+     * into the Hailo Executable Format (.hef).
+     *
+     * Targets: Hailo-8, Hailo-8L, Hailo-10, Hailo-15.
+     */
+    class HailoDriver : public ICompiler {
+    public:
+        bool compile(const CompileConfig& config) override;
+        bool validate_environment() override;
+
+        std::string get_name() const override {
+            return "Hailo Dataflow Compiler (DFC)";
+        }
+
+    private:
+        // Helper to generate the python build script
+        std::string generate_python_script(const CompileConfig& config,
+                                           const std::string& script_path,
+                                           const std::string& arch);
+    };
+
+} // namespace xinfer::compiler

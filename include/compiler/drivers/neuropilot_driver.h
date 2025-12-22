419ce8ea11028325d9a7d2ea137a67ccb129c56c
= <kamisaberi@gmail.com>
2025-12-23 00:24:26 +0330

    readme updated

--- include/compiler/drivers/neuropilot_driver.h
+++ include/compiler/drivers/neuropilot_driver.h
@@ -1,8 +1,25 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
 
-#ifndef XINFER_NEUROPILOT_DRIVER_H
-#define XINFER_NEUROPILOT_DRIVER_H
+#include <xinfer/compiler/base_compiler.h>
 
-#endif //XINFER_NEUROPILOT_DRIVER_H
+namespace xinfer::compiler {
+
+    /**
+     * @brief MediaTek NeuroPilot Compiler Driver
+     *
+     * Wraps the 'ncc' (Neuron Compiler) tool to convert models into
+     * MediaTek DLA binaries.
+     *
+     * Target: MediaTek Genio (IoT), Dimensity (Mobile).
+     */
+    class NeuroPilotDriver : public ICompiler {
+    public:
+        bool compile(const CompileConfig& config) override;
+        bool validate_environment() override;
+
+        std::string get_name() const override {
+            return "MediaTek NeuroPilot Compiler (ncc)";
+        }
+    };
+
+} // namespace xinfer::compiler

ace9644883cb2f9434b0a77564de416b502f7c60
= <kamisaberi@gmail.com>
2025-12-23 00:05:22 +0330

    readme updated

--- include/compiler/drivers/coreml_driver.h
+++ include/compiler/drivers/coreml_driver.h
@@ -1,8 +1,31 @@
-//
-// Created by kami on 12/22/2025.
-//
+#pragma once
 
-#ifndef XINFER_COREML_DRIVER_H
-#define XINFER_COREML_DRIVER_H
+#include <xinfer/compiler/base_compiler.h>
 
-#endif //XINFER_COREML_DRIVER_H
+namespace xinfer::compiler {
+
+    /**
+     * @brief Apple Core ML Compiler Driver
+     *
+     * Orchestrates the conversion of models (ONNX -> CoreML -> .mlmodelc).
+     * Uses 'xcrun coremlcompiler' for the final compilation step.
+     */
+    class CoreMLDriver : public ICompiler {
+    public:
+        bool compile(const CompileConfig& config) override;
+        bool validate_environment() override;
+
+        std::string get_name() const override {
+            return "Apple Core ML Compiler (coremlc)";
+        }
+
+    private:
+        /**
+         * @brief Helper to convert ONNX to intermediate .mlmodel using coremltools (Python)
+         */
+        bool convert_onnx_to_mlmodel(const std::string& onnx_path,
+                                     const std::string& mlmodel_path,
+                                     Precision precision);
+    };
+
+} // namespace xinfer::compiler

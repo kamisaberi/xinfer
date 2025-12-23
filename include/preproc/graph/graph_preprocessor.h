0252b1e42f642dbdb4bf4587372435919740fc15
= <kamisaberi@gmail.com>
2025-12-24 02:45:55 +0330

    some codes added

--- include/preproc/graph/graph_preprocessor.h
+++ include/preproc/graph/graph_preprocessor.h
@@ -1,8 +1,24 @@
-//
-// Created by kami on 12/24/2025.
-//
+#pragma once
+#include <xinfer/core/tensor.h>
+#include "types.h"
 
-#ifndef XINFER_GRAPH_PREPROCESSOR_H
-#define XINFER_GRAPH_PREPROCESSOR_H
+namespace xinfer::preproc {
 
-#endif //XINFER_GRAPH_PREPROCESSOR_H
+    class IGraphPreprocessor {
+    public:
+        virtual ~IGraphPreprocessor() = default;
+        virtual void init(const graph::GraphConfig& config) = 0;
+
+        /**
+         * @brief Convert list of edges to Graph Tensors.
+         *
+         * @param edges Vector of interactions (src, dst, weight).
+         * @param out_structure The structure tensor (Adjacency or Edge Index).
+         * @param out_weights The edge attribute tensor.
+         */
+        virtual void process(const std::vector<graph::Edge>& edges,
+                             core::Tensor& out_structure,
+                             core::Tensor& out_weights) = 0;
+    };
+
+}

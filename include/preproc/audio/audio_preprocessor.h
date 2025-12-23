ae880b0370ab7f4eb8cc59882fcbf13bb14af726
= <kamisaberi@gmail.com>
2025-12-23 13:19:39 +0330

    some codes added

--- include/preproc/audio/audio_preprocessor.h
+++ include/preproc/audio/audio_preprocessor.h
@@ -1,8 +1,28 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
+#include <xinfer/core/tensor.h>
+#include <vector>
 
-#ifndef XINFER_AUDIO_PREPROCESSOR_H
-#define XINFER_AUDIO_PREPROCESSOR_H
+namespace xinfer::preproc {
 
-#endif //XINFER_AUDIO_PREPROCESSOR_H
+    struct AudioBuffer {
+        const float* pcm_data; // Normalized float [-1.0, 1.0]
+        size_t num_samples;
+        int sample_rate;
+    };
+
+    struct AudioFeatureConfig {
+        bool use_mfcc = true;
+        int n_fft = 2048;
+        int hop_length = 512;
+        int n_mels = 128;
+    };
+
+    class IAudioPreprocessor {
+    public:
+        virtual ~IAudioPreprocessor() = default;
+
+        // Raw Audio -> Spectrogram/MFCC Tensor
+        virtual void process(const AudioBuffer& src, core::Tensor& dst) = 0;
+    };
+
+}

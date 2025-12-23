7bf7d428ba2d5980462d7f2dc6613de938901811
= <kamisaberi@gmail.com>
2025-12-23 14:07:02 +0330

    some codes added

--- include/preproc/factory.h
+++ include/preproc/factory.h
@@ -1,8 +1,44 @@
-//
-// Created by kami on 12/23/2025.
-//
+#pragma once
 
-#ifndef XINFER_FACTORY_H
-#define XINFER_FACTORY_H
+#include <memory>
 
-#endif //XINFER_FACTORY_H
+// Include the Interfaces
+#include <xinfer/preproc/image/image_preprocessor.h>
+#include <xinfer/preproc/audio/audio_preprocessor.h>
+
+// Include the Target definition
+// (Ideally, move 'Target' enum to xinfer/core/types.h so Preproc doesn't depend on Compiler)
+#include <xinfer/compiler/base_compiler.h>
+
+namespace xinfer::preproc {
+
+    /**
+     * @brief Factory for Image Preprocessors.
+     *
+     * Automatically selects the fastest implementation based on the target hardware.
+     *
+     * Strategy:
+     * - If Target == NVIDIA_TRT -> Returns CudaImagePreprocessor
+     * - If Target == ROCKCHIP_RKNN -> Returns RgaImagePreprocessor
+     * - If Target == APPLE_COREML -> Returns MetalImagePreprocessor
+     * - Else -> Returns OpenCVImagePreprocessor (CPU)
+     *
+     * @param target The hardware platform execution is running on.
+     * @return std::unique_ptr<IImagePreprocessor>
+     */
+    std::unique_ptr<IImagePreprocessor> create_image_preprocessor(xinfer::Target target);
+
+    /**
+     * @brief Factory for Audio Preprocessors.
+     *
+     * Strategy:
+     * - If Target == NVIDIA_TRT (and Batch > Threshold) -> Returns CuFFTAudioPreprocessor
+     * - If Target == APPLE_COREML -> Returns AccelerateAudioPreprocessor
+     * - Else -> Returns CpuAudioPreprocessor (Generic C++ / NEON)
+     *
+     * @param target The hardware platform execution is running on.
+     * @return std::unique_ptr<IAudioPreprocessor>
+     */
+    std::unique_ptr<IAudioPreprocessor> create_audio_preprocessor(xinfer::Target target);
+
+} // namespace xinfer::preproc

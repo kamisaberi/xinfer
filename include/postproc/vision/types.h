93710490f5bc8e5a5242c248ed8c5efdccecbb7d
= <kamisaberi@gmail.com>
2025-12-24 04:43:50 +0330

    some codes added

--- include/postproc/vision/types.h
+++ include/postproc/vision/types.h
@@ -1,8 +1,22 @@
-//
-// Created by kami on 12/24/2025.
-//
+#pragma once
+#include <vector>
 
-#ifndef XINFER_TYPES_H
-#define XINFER_TYPES_H
+namespace xinfer::postproc {
 
-#endif //XINFER_TYPES_H
+    struct BoundingBox {
+        float x1, y1, x2, y2;
+        float confidence;
+        int class_id;
+        // Optional: for 3D
+        float z = 0.0f, w = 0.0f, h = 0.0f, l = 0.0f, rot_y = 0.0f;
+    };
+
+    struct DetectionConfig {
+        float conf_threshold = 0.25f;
+        float nms_threshold = 0.45f;
+        int max_detections = 1000;
+        std::vector<float> anchors; // For YOLO
+        int num_classes = 80;
+    };
+
+}

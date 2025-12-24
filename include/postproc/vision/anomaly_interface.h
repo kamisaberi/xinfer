cb9013bdcfaf26d84395111cfd88bfce0127991a
kamran saberifard <kamisaberi@gmail.com>
2025-12-24 13:32:26 +0330

    xinfer is under construction

--- include/postproc/vision/anomaly_interface.h
+++ include/postproc/vision/anomaly_interface.h
@@ -1,8 +1,28 @@
-//
-// Created by kami on 12/24/2025.
-//
+#pragma once
+#include <xinfer/core/tensor.h>
+#include "types.h"
 
-#ifndef XINFER_ANOMALY_INTERFACE_H
-#define XINFER_ANOMALY_INTERFACE_H
+namespace xinfer::postproc {
 
-#endif //XINFER_ANOMALY_INTERFACE_H
+struct AnomalyConfig {
+    float threshold = 0.5f;     // Score above this is an anomaly
+    bool use_smoothing = true;  // Apply Gaussian Blur
+    int kernel_size = 7;        // Blur kernel size
+};
+
+struct AnomalyResult {
+    bool is_anomaly = false;
+    float anomaly_score = 0.0f;
+    core::Tensor heatmap;           // Float32 [0..1]
+    core::Tensor segmentation_mask; // Uint8 [0 or 255]
+};
+
+class IAnomalyPostprocessor {
+public:
+    virtual ~IAnomalyPostprocessor() = default;
+    virtual void init(const AnomalyConfig& config) = 0;
+    virtual AnomalyResult process(const core::Tensor& input, 
+                                  const core::Tensor& reconstruction) = 0;
+};
+
+}

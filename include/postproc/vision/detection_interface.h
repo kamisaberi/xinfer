5d1bbb8a21eed338df54959b226157e70957ac8b
= <kamisaberi@gmail.com>
2025-12-24 04:44:36 +0330

    some codes added

--- include/postproc/vision/detection_interface.h
+++ include/postproc/vision/detection_interface.h
@@ -1,8 +1,23 @@
-//
-// Created by kami on 12/24/2025.
-//
+#pragma once
+#include <xinfer/core/tensor.h>
+#include "types.h"
+#include <vector>
 
-#ifndef XINFER_DETECTION_INTERFACE_H
-#define XINFER_DETECTION_INTERFACE_H
+namespace xinfer::postproc {
 
-#endif //XINFER_DETECTION_INTERFACE_H
+    class IDetectionPostprocessor {
+    public:
+        virtual ~IDetectionPostprocessor() = default;
+
+        // Configure thresholds
+        virtual void init(const DetectionConfig& config) = 0;
+
+        /**
+         * @brief Decodes model output into bounding boxes.
+         * @param tensors The raw output from the inference engine.
+         *                (YOLO often produces 1 or 3 output tensors).
+         */
+        virtual std::vector<BoundingBox> process(const std::vector<core::Tensor>& tensors) = 0;
+    };
+
+}

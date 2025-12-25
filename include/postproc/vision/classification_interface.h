08856bd1b4cb6bcd869782b78622ef65c589d45d
= <kamisaberi@gmail.com>
2025-12-25 03:52:58 +0330

    some codes added

--- include/postproc/vision/classification_interface.h
+++ include/postproc/vision/classification_interface.h
@@ -1,8 +1,33 @@
-//
-// Created by kami on 12/25/2025.
-//
+#pragma once
+#include <xinfer/core/tensor.h>
+#include <vector>
+#include <string>
 
-#ifndef XINFER_CLASSIFICATION_INTERFACE_H
-#define XINFER_CLASSIFICATION_INTERFACE_H
+namespace xinfer::postproc {
 
-#endif //XINFER_CLASSIFICATION_INTERFACE_H
+    struct ClassResult {
+        int id;
+        float score;
+        std::string label; // Optional, populated if labels provided
+    };
+
+    struct ClassificationConfig {
+        int top_k = 5;       // Return top 5 results
+        bool apply_softmax = true; // If model returns raw logits
+        std::vector<std::string> labels; // Label map
+    };
+
+    class IClassificationPostprocessor {
+    public:
+        virtual ~IClassificationPostprocessor() = default;
+        virtual void init(const ClassificationConfig& config) = 0;
+
+        /**
+         * @brief Process classification logits.
+         * @param logits Tensor [Batch, NumClasses]
+         * @return Batch of vectors (one vector of Top-K results per image)
+         */
+        virtual std::vector<std::vector<ClassResult>> process(const core::Tensor& logits) = 0;
+    };
+
+}

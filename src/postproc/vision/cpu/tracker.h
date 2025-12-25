31eded70179efea2c9bcefbb8a2d6cab58ec0cb5
= <kamisaberi@gmail.com>
2025-12-25 04:04:34 +0330

    some codes added

--- src/postproc/vision/cpu/tracker.h
+++ src/postproc/vision/cpu/tracker.h
@@ -1,8 +1,33 @@
-//
-// Created by kami on 12/25/2025.
-//
+#pragma once
 
-#ifndef XINFER_TRACKER_H
-#define XINFER_TRACKER_H
+#include <xinfer/postproc/vision/tracker_interface.h>
+#include <vector>
+#include <memory>
 
-#endif //XINFER_TRACKER_H
+namespace xinfer::postproc {
+
+    /**
+     * @brief CPU Object Tracker (SORT - Simple Online and Realtime Tracking).
+     *
+     * Logic:
+     * 1. Kalman Filter predicts where objects should be in the next frame.
+     * 2. Hungarian/Greedy matching associates new detections to predictions via IoU.
+     * 3. Unmatched detections start new tracks.
+     * 4. Unmatched tracks age and eventually die.
+     */
+    class CpuTracker : public ITracker {
+    public:
+        CpuTracker();
+        ~CpuTracker() override;
+
+        void init(const TrackerConfig& config) override;
+        void reset() override;
+
+        std::vector<TrackedObject> update(const std::vector<BoundingBox>& detections) override;
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> m_impl; // PImpl to hide OpenCV headers
+    };
+
+} // namespace xinfer::postproc

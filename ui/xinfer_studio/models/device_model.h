a665bc9a8a3a0077ec424eb3553b28e428cdf4ba
= <kamisaberi@gmail.com>
2025-12-29 02:17:04 +0330

    some codes added

--- ui/xinfer_studio/models/device_model.h
+++ ui/xinfer_studio/models/device_model.h
@@ -1,8 +1,30 @@
-//
-// Created by kami on 12/29/2025.
-//
+#pragma once
 
-#ifndef XINFER_DEVICE_MODEL_H
-#define XINFER_DEVICE_MODEL_H
+#include <QAbstractTableModel>
+#include "../core/device_manager.h"
 
-#endif //XINFER_DEVICE_MODEL_H
+namespace xinfer::ui::models {
+
+    class DeviceModel : public QAbstractTableModel {
+        Q_OBJECT
+
+    public:
+        explicit DeviceModel(core::DeviceManager* manager, QObject* parent = nullptr);
+
+        // Required Overrides
+        int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+        int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+        QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+        QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+        // Helper to refresh UI when underlying data changes
+        void refresh();
+
+        // Get the full struct for a specific row
+        core::DeviceInfo getDevice(int row) const;
+
+    private:
+        core::DeviceManager* m_manager;
+    };
+
+} // namespace xinfer::ui::models

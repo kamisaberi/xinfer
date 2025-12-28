cb88ec0b2f4b2b57d2444ddb859ac6961ae16276
= <kamisaberi@gmail.com>
2025-12-29 01:54:01 +0330

    some codes added

--- ui/xinfer_studio/controllers/zoo_controller.h
+++ ui/xinfer_studio/controllers/zoo_controller.h
@@ -1,8 +1,35 @@
-//
-// Created by kami on 12/29/2025.
-//
+#pragma once
+#include <QObject>
+#include <QTimer>
+#include <opencv2/opencv.hpp>
 
-#ifndef XINFER_ZOO_CONTROLLER_H
-#define XINFER_ZOO_CONTROLLER_H
+// Include the Zoo
+#include <xinfer/zoo.h>
 
-#endif //XINFER_ZOO_CONTROLLER_H
+class ZooController : public QObject {
+    Q_OBJECT
+public:
+    explicit ZooController(QObject *parent = nullptr);
+    ~ZooController();
+
+public slots:
+    // Called by UI thread to start/stop
+    void startPipeline(const std::string& model_path, int camera_id);
+    void stopPipeline();
+
+private slots:
+    // Internal loop
+    void processLoop();
+
+    signals:
+        // Emitted to UI
+        void frameReady(const QImage& image);
+    void errorOccurred(QString message);
+
+private:
+    bool m_running = false;
+    cv::VideoCapture m_cap;
+
+    // The Zoo Module (Polymorphic wrapper could be used here)
+    std::unique_ptr<xinfer::zoo::vision::ObjectDetector> m_detector;
+};

38d8dfdd10f64139bd3779eb3d52505930e55a80
= <kamisaberi@gmail.com>
2025-12-29 02:21:54 +0330

    some codes added

--- ui/xinfer_studio/app/mainwindow.h
+++ ui/xinfer_studio/app/mainwindow.h
@@ -1,7 +1,14 @@
 #pragma once
+
 #include <QMainWindow>
 #include <QThread>
+#include <QImage>
+#include <QTimer>
+
+// Internal Components
 #include "../controllers/zoo_controller.h"
+#include "../core/device_manager.h"
+#include "../models/device_model.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,16 +22,43 @@ public:
     ~MainWindow();
 
 private slots:
+    // --- UI Interaction Slots (Auto-connected by name) ---
     void on_btnStart_clicked();
     void on_btnStop_clicked();
+    void on_btnBrowse_clicked();
+
+    // --- Menu Actions ---
+    void on_actionDevice_Manager_triggered();
+    void on_actionExit_triggered();
+
+    // --- Worker Thread Slots ---
+    /**
+     * @brief Receive a processed frame from the backend.
+     */
     void updateView(const QImage& image);
-    void handleError(QString msg);
+
+    /**
+     * @brief Receive log messages from the backend.
+     */
+    void appendLog(QString message);
+
+    /**
+     * @brief Handle critical errors from the backend.
+     */
+    void handleError(QString message);
 
 private:
     Ui::MainWindow *ui;
+
+    // Data Models
+    xinfer::ui::core::DeviceManager* m_deviceManager;
+    xinfer::ui::models::DeviceModel* m_deviceModel;
 
     // Threading
     QThread* m_workerThread;
     ZooController* m_controller;
 
+    // State
+    bool m_isPipelineRunning = false;
+    void updateUIState(bool running);
 };

f841580647e9eeadb8cefb372911a10134fe533f
= <kamisaberi@gmail.com>
2025-12-28 11:44:57 +0330

    some codes added

--- include/utils/downloader.h
+++ include/utils/downloader.h
@@ -1,8 +1,34 @@
-//
-// Created by kami on 12/28/2025.
-//
+#pragma once
 
-#ifndef XINFER_DOWNLOADER_H
-#define XINFER_DOWNLOADER_H
+#include <string>
+#include <functional>
 
-#endif //XINFER_DOWNLOADER_H
+namespace xinfer::utils {
+
+    /**
+     * @brief A utility for downloading files over HTTP/HTTPS.
+     *
+     * Uses libcurl for robust, cross-platform downloads.
+     */
+    class Downloader {
+    public:
+        /**
+         * @brief Downloads a file from a URL to a specified path.
+         *
+         * Features:
+         * - Skips download if file already exists.
+         * - Shows a command-line progress bar.
+         * - Verifies file integrity with an optional checksum.
+         *
+         * @param url The URL of the file to download.
+         * @param filepath The destination path to save the file.
+         * @param expected_sha256 Optional: The expected SHA256 checksum of the file.
+         *                        If provided and the checksum fails, the download fails.
+         * @return true if the file is successfully downloaded or already exists, false otherwise.
+         */
+        static bool download(const std::string& url,
+                             const std::string& filepath,
+                             const std::string& expected_sha256 = "");
+    };
+
+} // namespace xinfer::utils

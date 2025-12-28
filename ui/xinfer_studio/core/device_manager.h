2b3b1c78ce9c66fd7fa9024ec79d8e9554dfb350
= <kamisaberi@gmail.com>
2025-12-29 02:16:12 +0330

    some codes added

--- ui/xinfer_studio/core/device_manager.h
+++ ui/xinfer_studio/core/device_manager.h
@@ -1,8 +1,38 @@
-//
-// Created by kami on 12/29/2025.
-//
+#pragma once
 
-#ifndef XINFER_DEVICE_MANAGER_H
-#define XINFER_DEVICE_MANAGER_H
+#include <QString>
+#include <QList>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QFile>
 
-#endif //XINFER_DEVICE_MANAGER_H
+namespace xinfer::ui::core {
+
+    struct DeviceInfo {
+        QString name;
+        QString ip;
+        QString user;
+        QString target; // e.g. "nv-trt", "rockchip-rknn"
+        QString path;   // Remote path
+    };
+
+    class DeviceManager {
+    public:
+        explicit DeviceManager(const QString& jsonPath = "devices.json");
+
+        bool load();
+        bool save();
+
+        QList<DeviceInfo>& getDevices();
+        const QList<DeviceInfo>& getDevices() const;
+
+        void addDevice(const DeviceInfo& device);
+        void removeDevice(int index);
+
+    private:
+        QString m_jsonPath;
+        QList<DeviceInfo> m_devices;
+    };
+
+} // namespace xinfer::ui::core

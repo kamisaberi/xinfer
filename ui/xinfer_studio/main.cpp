6a6997bf88a286ebdb2c09daaa7d06244924982d
= <kamisaberi@gmail.com>
2025-12-29 06:01:37 +0330

    some codes added

--- ui/xinfer_studio/main.cpp
+++ ui/xinfer_studio/main.cpp
@@ -1,35 +1,25 @@
 #include "app/mainwindow.h"
+#include "helpers/stylesheet_loader.h"
 
 #include <QApplication>
-#include <QFile>
-#include <QTextStream>
-#include <QDir>
-#include <QDebug>
-
-// Helper to load stylesheet
-void apply_stylesheet(QApplication& app, const QString& path) {
-    QFile file(path);
-    if (file.open(QFile::ReadOnly | QFile::Text)) {
-        QTextStream stream(&file);
-        app.setStyleSheet(stream.readAll());
-        file.close();
-    } else {
-        qWarning() << "Could not find stylesheet:" << path;
-    }
-}
 
 int main(int argc, char *argv[])
 {
+    // High DPI Scaling for modern monitors
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
     QApplication app(argc, argv);
 
-    // Set Application Metadata
+    // Application Metadata
     app.setApplicationName("xInfer Studio");
     app.setOrganizationName("xInfer");
     app.setApplicationVersion("1.0.0");
 
     // Load Dark Theme
-    // We assume the qss file is copied to the build dir or in a resource file
-    apply_stylesheet(app, ":/styles/dark_theme.qss");
+    // Ensure resources.qrc is added to CMakeLists.txt
+    xinfer::ui::helpers::apply_theme(app, ":/styles/dark_theme.qss");
 
     MainWindow w;
     w.show();

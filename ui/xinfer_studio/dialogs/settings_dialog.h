0224e24014f9623bcc3adb7c2cc5f6ec4f9b8968
= <kamisaberi@gmail.com>
2025-12-29 04:50:50 +0330

    some codes added

--- ui/xinfer_studio/dialogs/settings_dialog.h
+++ ui/xinfer_studio/dialogs/settings_dialog.h
@@ -1,8 +1,26 @@
-//
-// Created by kami on 12/29/2025.
-//
+#pragma once
 
-#ifndef XINFER_SETTINGS_DIALOG_H
-#define XINFER_SETTINGS_DIALOG_H
+#include <QDialog>
+#include <QSettings>
 
-#endif //XINFER_SETTINGS_DIALOG_H
+namespace Ui { class SettingsDialog; }
+
+class SettingsDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit SettingsDialog(QWidget *parent = nullptr);
+    ~SettingsDialog();
+
+    // Static helper to get a setting anywhere in the app
+    static QString getSetting(const QString& key, const QString& defaultValue = "");
+
+private slots:
+    void on_btnSave_clicked();
+    void on_btnCancel_clicked();
+    void on_btnBrowseSdk_clicked();
+
+private:
+    Ui::SettingsDialog *ui;
+    void loadSettings();
+};

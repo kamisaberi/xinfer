bdb481460c1be41deb23d37fdebfa5656796c5a7
= <kamisaberi@gmail.com>
2025-12-29 05:14:49 +0330

    some codes added

--- ui/xinfer_studio/widgets/results_panel.h
+++ ui/xinfer_studio/widgets/results_panel.h
@@ -1,27 +1,23 @@
 #pragma once
 
 #include <QWidget>
-#include <QTreeWidget>
 #include <vector>
-
-// Include types to display
 #include <xinfer/postproc/vision/types.h>
+
+namespace Ui {
+    class ResultsPanel;
+}
 
 class ResultsPanel : public QWidget {
     Q_OBJECT
+
 public:
     explicit ResultsPanel(QWidget *parent = nullptr);
+    ~ResultsPanel();
 
-    // Clear the table
     void clear();
-
-    // Update with new detections
     void updateDetections(const std::vector<xinfer::postproc::BoundingBox>& boxes);
 
-    // Update with classification results
-    // void updateClassifications(...)
-
 private:
-    QTreeWidget* m_tree;
-    void setupUI();
+    Ui::ResultsPanel *ui;
 };

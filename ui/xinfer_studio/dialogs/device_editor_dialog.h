c598a2f0b78586fd1835682ca15d4fcbfe00e1f3
= <kamisaberi@gmail.com>
2025-12-29 05:52:32 +0330

    some codes added

--- ui/xinfer_studio/dialogs/device_editor_dialog.h
+++ ui/xinfer_studio/dialogs/device_editor_dialog.h
@@ -1,8 +1,27 @@
-//
-// Created by kami on 12/29/2025.
-//
+#pragma once
 
-#ifndef XINFER_DEVICE_EDITOR_DIALOG_H
-#define XINFER_DEVICE_EDITOR_DIALOG_H
+#include <QDialog>
+#include "../core/device_manager.h"
 
-#endif //XINFER_DEVICE_EDITOR_DIALOG_H
+namespace Ui { class DeviceEditorDialog; }
+
+class DeviceEditorDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    // Mode: Add New vs Edit Existing
+    explicit DeviceEditorDialog(QWidget *parent = nullptr);
+    explicit DeviceEditorDialog(const xinfer::ui::core::DeviceInfo& device, QWidget *parent = nullptr);
+    ~DeviceEditorDialog();
+
+    // Returns the struct populated from the UI fields
+    xinfer::ui::core::DeviceInfo getDeviceInfo() const;
+
+private slots:
+    void on_btnSave_clicked();
+    void on_btnCancel_clicked();
+
+private:
+    Ui::DeviceEditorDialog *ui;
+    void setupUiData(const xinfer::ui::core::DeviceInfo& device);
+};

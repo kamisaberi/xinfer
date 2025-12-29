05e191157dfc601e183f9c42360856954390c19b
= <kamisaberi@gmail.com>
2025-12-29 04:45:55 +0330

    some codes added

--- ui/xinfer_studio/views/deployment_view.h
+++ ui/xinfer_studio/views/deployment_view.h
@@ -1,8 +1,38 @@
-//
-// Created by kami on 12/29/2025.
-//
+#pragma once
 
-#ifndef XINFER_DEPLOYMENT_VIEW_H
-#define XINFER_DEPLOYMENT_VIEW_H
+#include <QWidget>
+#include "../controllers/compiler_controller.h"
+#include "../core/device_manager.h" // To populate target list
 
-#endif //XINFER_DEPLOYMENT_VIEW_H
+namespace Ui { class DeploymentView; }
+
+class DeploymentView : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit DeploymentView(QWidget *parent = nullptr);
+    ~DeploymentView();
+
+    // Inject dependencies
+    void setDeviceManager(xinfer::ui::core::DeviceManager* manager);
+
+private slots:
+    void on_btnBrowseInput_clicked();
+    void on_btnBrowseOutput_clicked();
+    void on_btnCompile_clicked();
+
+    // Updates from Controller
+    void onLog(QString msg);
+    void onProgress(int percent);
+    void onFinished(bool success, QString msg);
+
+private:
+    Ui::DeploymentView *ui;
+
+    QThread* m_workerThread;
+    CompilerController* m_controller;
+    xinfer::ui::core::DeviceManager* m_deviceManager;
+
+    void setupThread();
+    void populateTargets();
+};

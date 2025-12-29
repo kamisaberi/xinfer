7176e17dcdac37df29607c0d5de6aee339f6989f
= <kamisaberi@gmail.com>
2025-12-29 05:04:11 +0330

    some codes added

--- ui/xinfer_studio/helpers/stylesheet_loader.cpp
+++ ui/xinfer_studio/helpers/stylesheet_loader.cpp
@@ -1,3 +1,28 @@
-//
-// Created by kami on 12/29/2025.
-//
+#include "stylesheet_loader.h"
+
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+
+namespace xinfer::ui::helpers {
+
+    void apply_theme(QApplication& app, const QString& resourcePath) {
+        QFile file(resourcePath);
+
+        if (file.open(QFile::ReadOnly | QFile::Text)) {
+            QTextStream stream(&file);
+            QString styleSheet = stream.readAll();
+
+            // Apply to the global application instance
+            app.setStyleSheet(styleSheet);
+            file.close();
+
+            qInfo() << "[UI] Loaded theme successfully:" << resourcePath;
+        } else {
+            qWarning() << "[UI] Failed to load theme:" << resourcePath
+                       << "-- Error:" << file.errorString();
+        }
+    }
+
+} // namespace xinfer::ui::helpers

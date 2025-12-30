cf61d74e4511195cd264d4533b47feb245947adb
= <kamisaberi@gmail.com>
2025-12-30 11:35:28 +0330

    some codes added

--- include/flow/pipeline.h
+++ include/flow/pipeline.h
@@ -1,8 +1,47 @@
-//
-// Created by kami on 12/30/2025.
-//
+#pragma once
 
-#ifndef XINFER_PIPELINE_H
-#define XINFER_PIPELINE_H
+#include "node_interface.h"
+#include <string>
+#include <vector>
+#include <memory>
 
-#endif //XINFER_PIPELINE_H
+namespace xinfer::flow {
+
+    struct PipelineConfig {
+        std::string name;
+        int fps_limit = 0; // 0 = Uncapped
+    };
+
+    class Pipeline {
+    public:
+        Pipeline();
+        ~Pipeline();
+
+        /**
+         * @brief Load a pipeline from a JSON file.
+         *
+         * @param json_path Path to pipeline.json
+         * @return true if successful.
+         */
+        bool load(const std::string& json_path);
+
+        /**
+         * @brief Run the pipeline.
+         * Blocks until the pipeline finishes (EOS) or stop() is called.
+         */
+        void run();
+
+        /**
+         * @brief Stop execution.
+         */
+        void stop();
+
+        // Register custom nodes dynamically
+        static void register_node(const std::string& type_name, NodeCreator creator);
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> pimpl_;
+    };
+
+} // namespace xinfer::flow

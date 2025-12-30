54313e669f4776c4ee1db6055d2f43ca561a93d1
= <kamisaberi@gmail.com>
2025-12-30 11:35:16 +0330

    some codes added

--- include/flow/node_interface.h
+++ include/flow/node_interface.h
@@ -1,8 +1,44 @@
-//
-// Created by kami on 12/30/2025.
-//
+#pragma once
 
-#ifndef XINFER_NODE_INTERFACE_H
-#define XINFER_NODE_INTERFACE_H
+#include <string>
+#include <map>
+#include <memory>
+#include <any>
+#include <vector>
 
-#endif //XINFER_NODE_INTERFACE_H
+namespace xinfer::flow {
+
+    // Data passed between nodes
+    struct Packet {
+        std::map<std::string, std::any> data;
+        long long timestamp;
+        bool is_eos = false; // End of Stream
+    };
+
+    class INode {
+    public:
+        virtual ~INode() = default;
+
+        /**
+         * @brief Initialize the node with parameters from JSON.
+         */
+        virtual void init(const std::map<std::string, std::string>& params) = 0;
+
+        /**
+         * @brief Process a single packet.
+         *
+         * @param input Data from the previous node.
+         * @return Data to send to the next node (or empty Packet if filtering).
+         */
+        virtual Packet process(const Packet& input) = 0;
+
+        /**
+         * @brief Reset state (e.g. for trackers or temporal models).
+         */
+        virtual void reset() {}
+    };
+
+    // Factory type for creating nodes
+    using NodeCreator = std::function<std::unique_ptr<INode>()>;
+
+} // namespace xinfer::flow

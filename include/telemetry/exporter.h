915020c71221d1240defb9f524e0ea793b289c04
= <kamisaberi@gmail.com>
2026-01-02 17:56:12 +0330

    some codes added

--- include/telemetry/exporter.h
+++ include/telemetry/exporter.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <xinfer/telemetry/types.h>
 
+namespace xinfer::telemetry {
+
+    class IMetricExporter {
+    public:
+        virtual ~IMetricExporter() = default;
+
+        /**
+         * @brief Export system metrics to a destination.
+         */
+        virtual void export_metrics(const SystemMetrics& metrics) = 0;
+
+        /**
+         * @brief Export inference specific metrics.
+         */
+        virtual void export_inference(const InferenceMetrics& metrics) = 0;
+    };
+
+}

75eae9c1f2f64b8c7c51f8284a0d05c1a6e84aec
= <kamisaberi@gmail.com>
2026-04-02 01:56:43 +0330

    finally net came back

--- ui/xinfer_studio/helpers/qt_cv_converter.h
+++ ui/xinfer_studio/helpers/qt_cv_converter.h
@@ -3,8 +3,21 @@
 #include <opencv2/opencv.hpp>
 
 namespace xinfer::ui::helpers {
+
+
     /**
      * @brief Converts cv::Mat to QImage efficiently (zero-copy if possible).
+     * @brief Converts cv::Mat to QImage efficiently (zero-copy if possible).
+     * @brief Converts cv::Mat to QImage efficiently (zero-copy if possible).
+     * @brief Converts cv::Mat to QImage efficiently (zero-copy if possible).
+     * @brief Converts cv::Mat to QImage efficiently (zero-copy if possible).
+     * @brief Converts cv::Mat to QImage efficiently (zero-copy if possible).
+     * @brief Converts cv::Mat to QImage efficiently (zero-copy if possible).
      */
+
+
+
+
+
     QImage mat_to_qimage(const cv::Mat& mat);
 }
